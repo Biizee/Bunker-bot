a452ab2e6158df2943ed7f0c6e5be0b9b0c4262c
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-02-12 00:41:13 +0100

    Edited info command and users list

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 import random
 
 prof = ["a", "b", "c", "d"]
-users = []
+users = {}
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -24,26 +24,43 @@ async def on_ready():
     await bot.tree.sync()
     print(f"Бот запущен как {bot.user}")
 
-@bot.tree.command(name="info", description="Print to users dm his info")
-@app_commands.describe(user="Enter user")
-async def test(interaction: discord.Interaction, user: discord.User):
-    cp = prof.copy()
-    u_prof = random.choice(cp)
-    cp.remove(u_prof)
-    u_age = random.randint(1,10)
-    text = f"Your profession - {u_prof}. \nYour age - {u_age}."
-    await user.send(text)
-    await interaction.response.send_message("Test", ephemeral=True)
-    print(cp)
+@bot.tree.command(name="info", description="Send info to all users")
+@app_commands.describe(role="Select role")
+async def info(interaction: discord.Interaction, role: discord.Role):
+    guild = interaction.guild
+    sent = 0
+
+    for member in guild.members:
+        if role in member.roles and not member.bot:
+            #print(f"{member.name}")
+            cp = prof.copy()
+            u_prof = random.choice(cp)
+            u_age = random.randint(1, 10)
+
+            text = f"Your profession - {u_prof}.\nYour age - {u_age}."
+
+            try:
+                await member.send(text)
+                users.update({
+                    member.name:{
+                        "profession": {
+                            "value": u_prof,
+                            "hide": True
+                        },
+                        "age": {
+                            "value": u_age,
+                            "hide": True
+                        }
+                    }
+                })
+                #print(users[member.name]["profession"]["value"])
+                #users[member.name]["profession"]["hide"] = False
+                #print(users[member.name]["profession"])
+                sent += 1
+            except discord.Forbidden:
+                pass
+
+    await interaction.response.send_message(f"Sent messages to {sent} users with role **{role.name}**", ephemeral=True)
     users.append([u_prof, u_age])
-
-@bot.tree.command(name="test", description="For testing lists")
-@app_commands.describe(user="Enter user")
-async def list_t(interaction: discord.Interaction, user: discord.User):
-    print(users)
-    text = f"{users}"
-    await user.send(text)
-    await interaction.response.send_message("Test", ephemeral=True)
-
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
