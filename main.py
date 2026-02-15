6b17005d1378e5a676b12af51e82f71780871f5f
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-02-15 20:11:53 +0100

    Fixed some issues

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ async def update_info_text(user):
 @bot.tree.command(name="info", description="Send info to all users")
 @app_commands.describe(role="Select role")
 async def info(interaction: discord.Interaction, role: discord.Role):
+    await interaction.response.defer(ephemeral=True)
     users_list.clear()
     guild = interaction.guild
     sent = 0
@@ -107,7 +108,6 @@ async def info(interaction: discord.Interaction, role: discord.Role):
             except discord.Forbidden:
                 pass
 
-    print(dm_message)
     await interaction.response.send_message(f"Sent messages to {sent} users with role **{role.name}**", ephemeral=True)
 
 
@@ -115,10 +115,11 @@ async def info(interaction: discord.Interaction, role: discord.Role):
 @bot.tree.command(name="edit", description="Uncover info about urself")
 @app_commands.describe(info="Type category (profession / age)")
 async def edit(interaction: discord.Interaction, info: str):
+    await interaction.response.defer(ephemeral=True)
     user_name = interaction.user.name
 
     try:
-        users[user_name][info.lower]["hide"] = False
+        users[user_name][info.lower()]["hide"] = False
     except KeyError:
         await interaction.response.send_message("profession или age, дура",ephemeral=True)
         return
@@ -134,7 +135,6 @@ async def edit(interaction: discord.Interaction, info: str):
         for target in users_list:
             if target != other_user:
                 d = await update_info_text(target)
-                print(d)
                 text += f"""
 ===========================================
 {target}
